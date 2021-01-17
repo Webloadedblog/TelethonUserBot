ff17653a97b6f82537bfd8851f3d7bd39a77943e
Cee Jay <52139555+Somto811@users.noreply.github.com>
2021-01-17 19:59:11 +0100

    Create tmine.py

---
+++ userbot/modules/tmine.py
@@ -0,0 +1,17 @@
+from userbot.util import admin_cmd
+from telethon import events
+import asyncio
+TANNER = 79316791
+TCOIN = -1001394158904
+
+@bot.on(events.NewMessage(pattern="", incoming=True))
+async def handler(event):
+    me = await event.client.get_me()
+    if event.message.from_id != TANNER:
+        return
+    if "> exhausted miners:" not in event.message.message:
+        return
+    if me.first_name in event.message.message:
+        return
+    await asyncio.sleep(5)
+    await event.client.send_message(TCOIN, "!mine")
