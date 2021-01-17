a68a6bfb193b044292537b709d6a98da92b5c995
Cee Jay <52139555+Somto811@users.noreply.github.com>
2021-01-17 20:09:26 +0100

    Create clk.py

---
+++ userbot/modules/clk.py
@@ -0,0 +1,23 @@
+import random
+import string
+import requests
+from random import randint 
+from userbot.util import admin_cmd, client
+
+@bot.on(admin_cmd(pattern=r"^.enaclk", outgoing=True)
+async def enaclk(event):
+    await event.edit("K...")
+    @bot.on(admin_cmd(pattern=r"^http",incoming=True, func=lambda e: e.is_private)
+    async def clkstart(m):
+        person = await m.get_sender()
+        user=person.first_name
+        rantext = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
+        api_token = '32f9105a8194b5596482f0ed9631ab1483ec171e'
+        req = requests.get('https://urlshortx.com//api?api={}&url={}&alias={}'.format(api_token, m.text, rantext)).json()
+        if(req["status"] == 'error'):
+          smsg = req["message"]
+        else:
+          smsg = req["shortenedUrl"]
+      
+        sent = await m.reply(smsg)
+    await event.edit("Done...")
