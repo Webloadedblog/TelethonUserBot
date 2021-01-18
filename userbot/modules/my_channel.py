dd09200063878da9fa20b70e8dc910389ab835f5
Cee Jay <52139555+Somto811@users.noreply.github.com>
2021-01-18 19:33:02 +0100

    Add files via upload

---
+++ userbot/modules/my_channel.py
@@ -0,0 +1,8 @@
+from telethon import events
+
+@bot.on(events.NewMessage(incoming=True))
+async def transfer(event):
+    target_channel = -1001276100737
+    my_channel = -1001389391900
+    if event.chat_id == target_channel:
+        await borg.send_message(my_channel, event.message)
