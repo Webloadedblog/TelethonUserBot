f9bdcb502cb3ad4dd0653897188b64cf7db61667
Cee Jay <52139555+Somto811@users.noreply.github.com>
2021-01-20 19:01:32 +0100

    Update tmine.py

--- userbot/modules/tmine.py
+++ userbot/modules/tmine.py
@@ -1,17 +1,17 @@
 from userbot.util import admin_cmd
 from telethon import events
 import asyncio
-somto = 331210651
-TCOIN = -1001377775174
+TANNER = 79316791
+TCOIN = -1001394158904
 
 @bot.on(events.NewMessage(pattern="", incoming=True))
 async def handler(event):
     me = await event.client.get_me()
-    if event.message.from_id != somto:
+    if event.message.from_id != TANNER:
         return
     if "> exhausted miners:" not in event.message.message:
         return
     if me.first_name in event.message.message:
         return
-    await asyncio.sleep(10)
-    await event.client.send_message(TCOIN, "!mine")
+    await asyncio.sleep(5)
+    await event.client.send_message(TCOIN, "!mine-silent")
