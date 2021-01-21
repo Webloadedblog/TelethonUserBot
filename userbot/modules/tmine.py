915923414279641e733f49c97bcbc1cc7810162a
Cee Jay <52139555+Somto811@users.noreply.github.com>
2021-01-21 08:46:34 +0100

    Update tmine.py

--- userbot/modules/tmine.py
+++ userbot/modules/tmine.py
@@ -4,14 +4,14 @@ import asyncio
 TANNER = 79316791
 TCOIN = -1001394158904
 
-@bot.on(events.NewMessage(pattern="", incoming=True))
+@bot.on(events.NewMessage(incoming=True))
 async def handler(event):
     me = await event.client.get_me()
     if event.message.from_id != TANNER:
         return
     if "> exhausted miners:" not in event.message.message:
         return
-    if me.first_name in event.message.message:
+    if "cee jay" in event.message.message:
         return
     await asyncio.sleep(5)
     await event.client.send_message(TCOIN, "!mine-silent")
