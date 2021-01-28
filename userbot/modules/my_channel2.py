405557e99487aaf7e54e3161e34587bafded3934
Webloadedblog <54663208+Webloadedblog@users.noreply.github.com>
2021-01-28 01:20:44 +0100

    Update my_channel2.py

--- userbot/modules/my_channel2.py
+++ userbot/modules/my_channel2.py
@@ -4,6 +4,5 @@ from telethon import events
 async def transfer(event):
     target_channel = -1001288631370
     my_channel = -1001371876881
-    event.message.text = "\n".join(event.message.text.split("\n")[:1])
-    if event.chat_id == target_channel:
+   if event.chat_id == target_channel:
         await borg.send_message(my_channel, event.message)
