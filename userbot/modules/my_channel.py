17b6b0192d21a7d4f0b2f98e9f896a7921d60d8f
Webloadedblog <54663208+Webloadedblog@users.noreply.github.com>
2021-01-28 01:47:42 +0100

    Delete my_channel.py

--- userbot/modules/my_channel.py
+++
@@ -1,8 +0,0 @@
-from telethon import events
-
-@bot.on(events.NewMessage(incoming=True))
-async def transfer(event):
-    target_channel = -1001473633317
-    my_channel = -1001371876881
-    if event.chat_id == target_channel:
-        await borg.send_message(my_channel, event.message)
